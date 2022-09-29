470bc58aa54f80db59bdb4e92e9bc56b1be240e0
NinoosMoshi <nalkaton@gmail.com>
2022-09-29 10:35:32 -0400

    add controller

---
+++ spring-security/src/main/java/com/ninos/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.ninos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/security")
+public class StudentController {
+
+
+    @GetMapping("/student")
+    public String hiStudent(){
+        return "hi student";
+    }
+
+
+
+
+}
